c6f96d151d50332f0fb271158b2ac82cad8e1485
avasilistov <avasilistov@gmail.com>
2019-12-24 18:46:29 +0200

    tcp client with async.io

---
+++ asyncio_tcp_client.py
@@ -0,0 +1,17 @@
+import asyncio
+
+
+async def tcp_echo_client(message, loop):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1',
+        10001,
+        loop=loop)
+    print('send: %r' % message)
+    writer.write(message.encode())
+    writer.close()
+
+
+loop = asyncio.get_event_loop()
+message = 'hello World!'
+loop.run_until_complete(tcp_echo_client(message, loop))
+loop.close()
