c6bbcd11b5eec67724f3e3cd81bf55635ac11397
avasilistov <avasilistov@gmail.com>
2019-12-24 18:24:13 +0200

    Print hello world with async.io

---
+++ asyncio_tcp.py
@@ -0,0 +1,25 @@
+import asyncio
+
+
+async def handle_echo(reader, writer):
+    data = await reader.read(1024)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print('received %r from %r' % (message, addr))
+    writer.close()
+
+
+loop = asyncio.get_event_loop()
+coru = asyncio.start_server(handle_echo,
+                            '127.0.0.1',
+                            10001,
+                            loop=loop)
+server = loop.run_until_complete(coru)
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
